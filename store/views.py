bc09507dadd936eb7f28d332a9f057503a206331
Angelica <angelica.sotis@carsu.edu.ph>
2022-06-21 17:11:16 +0800

    updated

--- store/views.py
+++ store/views.py
@@ -1,4 +1,3 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -14,12 +13,12 @@ def products(request):
 
 def view_products(request):
     context = {}
-    return render(request, 'store/view_products.html', context)
+    return render(request, 'store/green_tea.html', context)
 
 
 def view_products_details(request):
     context = {}
-    return render(request, 'store/view_products_details.html', context)
+    return render(request, 'store/green_tea_details.html', context)
 
 
 def cart(request):
@@ -69,7 +68,7 @@ def verification(request):
 
 def reset_password(request):
     context = {}
-    return render(request, 'Register_login/verification.html', context)
+    return render(request, 'Register_login/reset_password.html', context)
 
 
 def successfully_reset(request):
@@ -80,3 +79,194 @@ def successfully_reset(request):
 def reviews(request):
     context = {}
     return render(request, 'store/reviews.html', context)
+
+
+def butterfly(request):
+    context = {}
+    return render(request, 'view_products/butterfly.html', context)
+
+
+def butterfly_details(request):
+    context = {}
+    return render(request, 'view_products/butterfly_details.html', context)
+
+
+def chamomile(request):
+    context = {}
+    return render(request, 'view_products/chamomile.html', context)
+
+
+def chamomile_details(request):
+    context = {}
+    return render(request, 'view_products/chamomile_details.html', context)
+
+
+def green_tea(request):
+    context = {}
+    return render(request, 'view_products/green_tea.html', context)
+
+
+def green_tea_details(request):
+    context = {}
+    return render(request, 'view_products/green_tea_details.html', context)
+
+
+def lapsang(request):
+    context = {}
+    return render(request, 'view_products/lapsang.html', context)
+
+
+def lapsang_details(request):
+    context = {}
+    return render(request, 'view_products/lapsang_details.html', context)
+
+
+# user_view
+def user_home(request):
+    context = {}
+    return render(request, 'user/home.html', context)
+
+
+def user_products(request):
+    context = {}
+    return render(request, 'user/products.html', context)
+
+
+def user_view_products(request):
+    context = {}
+    return render(request, 'user/green_tea.html', context)
+
+
+def user_view_products_details(request):
+    context = {}
+    return render(request, 'user/green_tea_details.html', context)
+
+
+def user_cart(request):
+    context = {}
+    return render(request, 'user/cart.html', context)
+
+
+def user_checkout(request):
+    context = {}
+    return render(request, 'user/checkout.html', context)
+
+
+def user_faqs(request):
+    context = {}
+    return render(request, 'user/faqs.html', context)
+
+
+def user_about_us(request):
+    context = {}
+    return render(request, 'user/about_us.html', context)
+
+
+def user_contact_us(request):
+    context = {}
+    return render(request, 'user/contact_us.html', context)
+
+
+def user_register(request):
+    context = {}
+    return render(request, 'Register_login/register.html', context)
+
+
+def user_login(request):
+    context = {}
+    return render(request, 'Register_login/login.html', context)
+
+
+def user_reset(request):
+    context = {}
+    return render(request, 'Register_login/reset.html', context)
+
+
+def user_verification(request):
+    context = {}
+    return render(request, 'Register_login/verification.html', context)
+
+
+def user_reset_password(request):
+    context = {}
+    return render(request, 'Register_login/reset_password.html', context)
+
+
+def user_successfully_reset(request):
+    context = {}
+    return render(request, 'Register_login/successfully_reset.html', context)
+
+
+def user_reviews(request):
+    context = {}
+    return render(request, 'user/reviews.html', context)
+
+
+def user_butterfly(request):
+    context = {}
+    return render(request, 'user_view_products/butterfly.html', context)
+
+
+def user_butterfly_details(request):
+    context = {}
+    return render(request, 'user_view_products/butterfly_details.html', context)
+
+
+def user_chamomile(request):
+    context = {}
+    return render(request, 'user_view_products/chamomile.html', context)
+
+
+def user_chamomile_details(request):
+    context = {}
+    return render(request, 'user_view_products/chamomile_details.html', context)
+
+
+def user_green_tea(request):
+    context = {}
+    return render(request, 'user_view_products/green_tea.html', context)
+
+
+def user_green_tea_details(request):
+    context = {}
+    return render(request, 'user_view_products/green_tea_details.html', context)
+
+
+def user_lapsang(request):
+    context = {}
+    return render(request, 'user_view_products/lapsang.html', context)
+
+
+def user_lapsang_details(request):
+    context = {}
+    return render(request, 'user_view_products/lapsang_details.html', context)
+
+
+def cash_on(request):
+    context = {}
+    return render(request, 'payments/cash_on.html', context)
+
+
+def credit_debit(request):
+    context = {}
+    return render(request, 'payments/credit_debit.html', context)
+
+
+def credit_debit_ready_to_place(request):
+    context = {}
+    return render(request, 'payments/credit_debit_ready_to_place.html', context)
+
+
+def e_wallet(request):
+    context = {}
+    return render(request, 'payments/e_wallet.html', context)
+
+
+def to_receive(request):
+    context = {}
+    return render(request, 'purchase/to_receive.html', context)
+
+
+def purchase_details(request):
+    context = {}
+    return render(request, 'purchase/purchase_details.html', context)
